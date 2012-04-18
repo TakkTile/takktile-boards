4df8a2cf022479a3cc2060ddc5e498db6c664cca
Ian Daniher <it.daniher@gmail.com>
2012-04-18 16:50:05 -0400

    added pindefs from schematic

---
+++ pindefs.h
@@ -0,0 +1,11 @@
+#define RST0 PA0
+#define RST1 PA1
+#define RST2 PA2
+#define RST3 PA3
+#define SCL PA4
+#define RST4 PA5
+#define SDA PA6
+#define PA7 ADDR3
+#define PB0 ADDR0
+#define PB1 ADDR1
+#define PB2 ADDR2
