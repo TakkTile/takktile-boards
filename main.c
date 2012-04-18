f49bfb9b76f05dc203ad9dd2688c319a14edee8b
Ian Daniher <it.daniher@gmail.com>
2012-04-18 17:37:49 -0400

    added configPins, a bunch of #defines, and code to calculate base I2C address

--- main.c
+++ main.c
@@ -4,16 +4,48 @@
 #include <util/delay.h>
 #include "usiTwiSlave.h"
 
+#define RST0 PA0
+#define RST1 PA1
+#define RST2 PA2
+#define RST3 PA3
+#define SCL PA4
+#define RST4 PA5
+#define SDA PA6
+#define PA7 ADDR3
+#define PB0 ADDR0
+#define PB1 ADDR1
+#define PB2 ADDR2
 
-int main(void)
-{
+/*
+	DDRA - port direction configuration
+	PORTA - port output configuration
+	PINA - port status
+*/
+
+void configPins(void) {
+
+	// disable all MPL115A2 sensors
+	DDRA |= (1 << RST0) | (1 << RST1) | (1 << RST2) | (1 << RST3) | (1 << RST4); // all 
+	PORTA &= ~((1 << RST0) | (1 << RST1) | (1 << RST2) | (1 << RST3) | (1 << RST4));
+
+	// set all ADDR pins as inputs with pullups
+	DDRA ^= (1 << ADDR3);
+	DDRB ^= (1 << ADDR0) | (1 << ADDR1) | ( 1 << ADDR2);
+	PORTA |= (1 << ADDR3);
+	PORTB |= (1 << ADDR0) | (1 << ADDR1) | ( 1 << ADDR2);
+}
+
+int main(void) {
+	configPins();
+	// calculate slaveAddress from state of ADDR pins, shift
+	uint8_t slaveAddress = (PINA & ADDR3) << 3 | (PINB & ADDR2) << 2 | (PINB & ADDR1) << 1 | (PINB & ADDR0) << 0;
+	slaveAddress <<= 4;
+
 	// enable interupts	
 	sei(); 
 	
-	const uint8_t slaveAddress = 0x14;
 	usiTwiSlaveInit(slaveAddress);
-	for(;;)
-	{
+	for(;;) {
 		if(usiTwiDataInReceiveBuffer())
 		{
 			uint8_t value;
