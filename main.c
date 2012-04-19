7816e2412dde72bf1a0eecbc3c64e7c872fda5f0
Ian Daniher <it.daniher@gmail.com>
2012-04-18 20:36:47 -0400

    wow, iotnx4.h is *useless*

--- main.c
+++ main.c
@@ -4,17 +4,17 @@
 #include <util/delay.h>
 #include "usiTwiSlave.h"
 
-#define RST0 PA0
-#define RST1 PA1
-#define RST2 PA2
-#define RST3 PA3
-#define SCL PA4
-#define RST4 PA5
-#define SDA PA6
-#define ADDR3 PA7
-#define ADDR0 PB0
-#define ADDR1 PB1
-#define ADDR2 PB2
+#define RST0 1 << PA0
+#define RST1 1 << PA1
+#define RST2 1 << PA2
+#define RST3 1 << PA3
+#define SCL 1 << PA4
+#define RST4 1 << PA5
+#define SDA 1 << PA6
+#define ADDR3 1 << PA7
+#define ADDR0 1 << PB0
+#define ADDR1 1 << PB1
+#define ADDR2 1 << PB2
 
 /*
 	DDRA - port direction configuration
@@ -25,14 +25,14 @@
 void configPins(void) {
 
 	// disable all MPL115A2 sensors
-	DDRA |= (1 << RST0) | (1 << RST1) | (1 << RST2) | (1 << RST3) | (1 << RST4); // all 
-	PORTA &= ~((1 << RST0) | (1 << RST1) | (1 << RST2) | (1 << RST3) | (1 << RST4));
+	DDRA |= RST0 | RST1 | RST2 | RST3 | RST4; // all 
+	PORTA &= ~(RST0 | RST1 | RST2 | RST3 | RST4);
 
 	// set all ADDR pins as inputs with pullups
-	DDRA ^= (1 << ADDR3);
-	DDRB ^= (1 << ADDR0) | (1 << ADDR1) | ( 1 << ADDR2);
-	PORTA |= (1 << ADDR3);
-	PORTB |= (1 << ADDR0) | (1 << ADDR1) | ( 1 << ADDR2);
+	DDRA ^= ADDR3;
+	DDRB ^= ADDR0 | ADDR1 | ADDR2;
+	PORTA |= ADDR3;
+	PORTB |= ADDR0 | ADDR1 | ADDR2;
 }
 
 int main(void) {
